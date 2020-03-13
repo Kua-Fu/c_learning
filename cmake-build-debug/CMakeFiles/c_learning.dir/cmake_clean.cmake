5d53b13bc8995434d23d64250df3d23e9679ed80
thewind <15951606335@163.com>
2020-03-13 10:36:51 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/c_learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/c_learning.dir/main.c.o"
+  "c_learning.pdb"
+  "c_learning"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/c_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
